d3297d71947ae799a5d0db795690c39e856d0808
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 02:26:50 +0700

    berkenalan dengan mustaches dan v-text

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        mesage: 'pacu pacu sik berajah, sudah cukup sekali saja kesalahan yang pernah kamu lakukan dalam hidupmu'
+        mesage: 'pacu gamakne sik berajah, sudah cukup sekali saja kesalahan yang pernah kamu lakukan dalam hidupmu'
     }
 })
