cbb9953afe1b1b9e4dd010209e8c6004325177eb
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 05:36:47 +0700

    belajar directive v-if

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        // pada variabel text merupakan tipe data string
-        text: 'khairul',
-        // pada variabel angka merupakan tipe data int
-        angka: 25,
-        // pada variabel angkakoma merupakan tipe data float
-        angkakoma: 2.5,
-        // pada variabel pilihanbenar merupakan tipe data bolean
-        pilihanbenar: true,
-        // pada variabel pilihansalah  juga merupakan tipe data bolean
-        pilihansalah: false,
-        // pada variabel aray merupakan tipe data aray
-        aray: ['aku', 'hanya', 'mencintai', 1, 'orang']
+        // jika nilai tampil adalah true maka akan menampilkan nilai pesan 
+        tampil: true,
+        pesan: 'aku akan membiming mu sampai surga'
     }
 })
