f52ec4579d4f347d5b0a538e3e76618549bf2aaa
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 05:00:43 +0700

    menampilkan multidata

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         // ketika ingin memasukan format dalam isi variabel (pesan: '<b>hello  word</b>')
-        pesan: '<b>hello  word</b>'
+        pesan: 'hallo khairul aku rindu kamu',
+        pesan1: 'khairul!',
+        pesan2: 'kahfie'
     }
 })
