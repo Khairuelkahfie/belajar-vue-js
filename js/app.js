afaacc13007c1ab72a91238b5f490881f511c051
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 01:22:08 +0700

    belajar vue js sambil memahami github

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        mesage: 'hello word!'
+        mesage: 'pacu pacu sik berajah, sudah cukup sekali saja kesalahan yang pernah kamu lakukan dalam hidupmu'
     }
 })
