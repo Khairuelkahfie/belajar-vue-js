44f688efc192c2737c0249f921724221f4b7de95
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 19:18:57 +0700

    gabungan property computed dan method

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 var app = new Vue({
     el: '#app',
     data: {
-        aku: 1,
-        kamu: 1,
-        jumlah: null,
+        aku: 2,
+        kamu: 5,
+        tambah: null,
+        kurangi: null,
+        kalian: null,
+        bagian: null
+    },
+    computed: {
+        jumlah: function () {
+            return this.aku + this.kamu
+        },
+        kurang: function () {
+            return this.aku - this.kamu
+        },
+        kali: function () {
+            return this.aku * this.kamu
+        },
+        bagi: function () {
+            return this.aku / this.kamu
+        }
     },
     methods: {
         nikahkan: function () {
-            return this.jumlah = this.aku + this.kamu
+            this.tambah = this.aku + this.kamu,
+                this.kurangi = this.aku - this.kamu,
+                this.kalian = this.aku * this.kamu,
+                this.bagian = this.aku / this.kamu
         }
     }
 })
