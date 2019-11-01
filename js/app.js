22096a4572682b541d89edf7ed5e8c9a03a1f3d7
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-01 17:59:31 +0700

    menggunakan computed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        // jika nilai tampil sama dengan false maka akan tampil pesan1
-        // jikan nilai tampil sama dengan true maka akan tampil pesan
-        tampil: true,
-        pesan: 'aku akan menikahim mu dan membiming mu sampai surga',
-        pesan1: 'aku akan pergi'
+        aku: 1,
+        kamu: 1
+    },
+    computed: {
+        jumlah: function () {
+            return this.aku + this.kamu
+        }
     }
 })
