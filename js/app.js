9e0dd7e160d38f477723c335e9d10a1cab741bfd
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-03 20:02:31 +0700

    implementasi watcher dan methods di form login

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isi: '',
-        keterangan: ''
+        isi1: '',
+        isi2: '',
+        keterangan: '',
+        tampil: false
     },
     watch: {
-        isi: 'ketik'
+        isi2: 'ketik'
     },
     methods: {
         ketik: function () {
-            if (this.isi == 'cinta') {
-                this.keterangan = 'aku juga cinta kamu'
+            if (this.isi2 == this.isi1) {
+                this.keterangan = '<i class="text-success">hati kita selalu sama</i>';
+                this.tampil = true;
             } else {
-                this.keterangan = 'sedang mengetik'
+                this.keterangan = '<i class="text-danger">hati kita berbeda</i>'
+                this.tampil = false;
             }
-
         }
     }
 })
