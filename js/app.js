7041333e7188eaa7df95fa328f67890d567f0f3d
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-03 19:01:26 +0700

    wacther murni

--- js/app.js
+++ js/app.js
@@ -1,33 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        aku: 2,
-        kamu: 5,
-        tambah: null,
-        kurangi: null,
-        kalian: null,
-        bagian: null
+        isi: '',
+        keterangan: ''
     },
-    computed: {
-        jumlah: function () {
-            return this.aku + this.kamu
-        },
-        kurang: function () {
-            return this.aku - this.kamu
-        },
-        kali: function () {
-            return this.aku * this.kamu
-        },
-        bagi: function () {
-            return this.aku / this.kamu
-        }
-    },
-    methods: {
-        nikahkan: function () {
-            this.tambah = this.aku + this.kamu,
-                this.kurangi = this.aku - this.kamu,
-                this.kalian = this.aku * this.kamu,
-                this.bagian = this.aku / this.kamu
+    watch: {
+        isi: function () {
+            this.keterangan = "sedang mengetik"
         }
     }
 })
