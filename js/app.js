b92080355fc5b3e10417a9089f8c5a783366be55
KhairuelKahfie <khairoelkahfie@gmail.com>
2019-11-03 22:26:09 +0700

    bermain dengan form

--- js/app.js
+++ js/app.js
@@ -1,23 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isi1: '',
-        isi2: '',
-        keterangan: '',
-        tampil: false
-    },
-    watch: {
-        isi2: 'ketik'
-    },
-    methods: {
-        ketik: function () {
-            if (this.isi2 == this.isi1) {
-                this.keterangan = '<i class="text-success">hati kita selalu sama</i>';
-                this.tampil = true;
-            } else {
-                this.keterangan = '<i class="text-danger">hati kita berbeda</i>'
-                this.tampil = false;
-            }
-        }
+        pesan: ''
     }
+
 })
